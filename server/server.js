7747594a0ba34f8d7ee8a76e67f4e1c820f68908
estermer <eric.stermer@gmail.com>
2016-12-12 14:59:44 -0500

    added body and cookie parsers/ required them in the server/ set up express confirg/ opened up app to listen on port 3000

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 ///MODULES///
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
 ///********///
+
+///EXPRESS CONFIG///
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+app.use( bodyParser.json() );    // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({  // to support URL-encoded bodies
+  extended: true
+}));
+///***********///
 
 ///MONGOOSE///
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/customers';
 mongoose.connect(mongoURI);
 mongoose.Promise = global.Promise;
 ///********///
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('=============================');
+  console.log('SERVER CONNECTED TO PORT 3000');
+  console.log('=============================');
+});
